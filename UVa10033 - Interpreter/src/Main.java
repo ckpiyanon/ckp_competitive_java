68b10f26a0b6e3d58741282f1591aa78e224c06d
Chinakrit Lorpiyanon <Chinakrit Lorpiyanon>
2016-07-15 09:00:36 +0700

    Accepted UVa10033 - Interpreter

--- UVa10033 - Interpreter/src/Main.java
+++ UVa10033 - Interpreter/src/Main.java
@@ -1,5 +1,4 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
@@ -10,38 +9,48 @@ public class Main {
 		for(int i = 0;i < p;i++)	n /= 10;
 		return n % 10;
 	}
-	static int exec(int cmd) {
-		System.out.printf("%03d\n",cmd);
-		int d = getNum(cmd,1),s = getNum(cmd,0),n = s;
-		switch(getNum(cmd,2)) {
-		case 0: return register[s] == 0 ? 1:(1 + exec(register[d]));
-		case 1: return (d == 0 && s == 0) ? 1:0;
-		case 2: register[d] = s; break;
-		case 3: register[d] = (register[d] + n) % 1000; break;
-		case 4: register[d] = (register[d] * n) % 1000; break;
-		case 5: register[d] = register[s]; break;
-		case 6: register[d] = (register[d] + register[s]) % 1000; break;
-		case 7: register[d] = (register[d] * register[s]) % 1000; break;
-		case 8: register[d] = ram[register[s]]; break;
-		case 9: ram[register[d]] = register[s]; break;
-		}
-		return 1;
-	}
+	static boolean halt;
 	public static void main(String args[]) throws Exception {
-		System.setIn(new FileInputStream("in.txt"));
+//		System.setIn(new FileInputStream("in.txt"));
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		String sin;
-		int TC = Integer.parseInt(in.readLine()),i,ans;
+		int TC = Integer.parseInt(in.readLine()),i,cmd,ans;
 		in.readLine();
 		while(TC-- > 0) {
 			Arrays.fill(register,0);
 			Arrays.fill(ram,0);
+			halt = false;
+			i = 0;
+			while((sin = in.readLine()) != null && sin.length() > 0)
+				ram[i++] = Integer.parseInt(sin);
 			i = ans = 0;
-			while((sin = in.readLine()) != null && sin.length() > 0) {
-				System.out.println(Arrays.toString(register));
-				ram[i] = Integer.parseInt(sin);
-				ans += exec(ram[i]);
-				i++;
+			while(true) {
+				cmd = ram[i];
+//				System.out.println(Arrays.toString(register));
+//				System.out.println(cmd);
+				if(cmd == 100) {
+					ans++;
+					break;
+				}
+				else {
+					int c = getNum(cmd,2),d = getNum(cmd,1),s = getNum(cmd,0);
+					switch(c) {
+					case 1: i++; continue;
+					case 2: register[d] = s; break;
+					case 3: register[d] = (register[d] + s) % 1000; break;
+					case 4: register[d] = (register[d] * s) % 1000; break;
+					case 5: register[d] = register[d] = register[s]; break;
+					case 6: register[d] = (register[d] + register[s]) % 1000; break;
+					case 7: register[d] = (register[d] * register[s]) % 1000; break;
+					case 8: register[d] = ram[register[s]]; break;
+					case 9: ram[register[s]] = register[d]; break;
+					case 0: {
+						if(register[s] != 0)	i = register[d] - 1;
+					}
+					}
+					ans++;
+					i++;
+				}
 			}
 			System.out.println(ans);
 			if(TC > 0)	System.out.println();
