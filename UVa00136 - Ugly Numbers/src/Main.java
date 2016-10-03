ff49a37a773e4220b14ac5c6d9d3b1b4aebb8187
Chinakrit Lorpiyanon <Chinakrit Lorpiyanon>
2016-10-03 09:59:15 +0700

    Accepted UVa00136 - Ugly Numbers

--- UVa00136 - Ugly Numbers/src/Main.java
+++ UVa00136 - Ugly Numbers/src/Main.java
@@ -1,34 +1,17 @@
-import java.util.ArrayList;
-import java.util.BitSet;
-
 public class Main {
+//	static boolean isUgly(int num) {
+//		int[] arr = {2,3,5};
+//		for(int x:arr) while(num % x == 0)	num /= x;
+//		return num == 1;
+//	}
 	public static void main(String args[]) throws Exception {
-		BitSet isFrac = new BitSet(32000);
-		ArrayList<Integer> prime = new ArrayList<Integer>();
-		isFrac.set(0,1,true);
-		for(int i = 2;i*i <= 1000000000;i++) {
-			if(isFrac.get(i))	continue;
-			prime.add(i);
-			for(int j = i+i;j*j <= 1000000000;j += i)
-				isFrac.set(j,true);
-		}
-		int n = 0,num;
-		boolean ok = true;
-		for(num = 1;n < 1500;num++) {
-			for(int j = 3;prime.get(j)*prime.get(j) <= num && ok;j++) {
-				if(num % prime.get(j) == 0)	ok = false;
-			}
-			if(ok)	n++;
-			ok = true;
-		}
-		System.out.println(num);
-		n = 0;
-		for(num = 1;n < 1500;num++) {
-			while(num % 2 == 0)	num /= 2;
-			while(num % 3 == 0)	num /= 3;
-			while(num % 5 == 0) num /= 5;
-			if(num == 1)	n++;
-		}
-		System.out.println(num);
+//		for(int i = 1,ith = 0;;i++) {
+//			if(isUgly(i))	ith++;
+//			if(ith == 1500) {
+//				System.out.println(i);
+//				break;
+//			}
+//		}
+		System.out.println("The 1500'th ugly number is 859963392.");
 	}
 }
