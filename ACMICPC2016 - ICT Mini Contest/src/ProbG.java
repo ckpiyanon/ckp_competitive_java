1eb5a216ef1cb9097724d0dda6af8d68fce44d7e
Chinakrit Lorpiyanon <Chinakrit Lorpiyanon>
2016-10-07 03:24:07 +0700

    edit all 2-sat problems: use Sat class

--- ACMICPC2016 - ICT Mini Contest/src/ProbG.java
+++ ACMICPC2016 - ICT Mini Contest/src/ProbG.java
@@ -16,57 +16,61 @@ public class ProbG {
 		try {System.setIn(new FileInputStream("inG.txt"));} catch(Exception e) {}
 		try {System.setOut(new PrintStream(new FileOutputStream("outG.txt")));} catch(Exception e) {}
 		st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
-		for(int i = 0;i < 1000;i++)	graph.add(new ArrayList<Integer>());
-		int TC = getInt(),n,x,y,a,b,dA,dB;
-		boolean yes;
+		int TC = getInt(),n;
 		while(TC-- > 0) {
-			for(ArrayList<Integer> each:graph)	each.clear();
-			x = getInt(); y = getInt(); n = y*2;
-			while(x-- > 0) {
-				a = getInt(); b = getInt();
-				dA = a > 0 ? 1:0; dB = b > 0 ? 1:0;
-				a = Math.abs(a) - 1; b = Math.abs(b) - 1;
-				graph.get(a*2 + (dA^1)).add(b*2 + dB);
-				graph.get(b*2 + (dB^1)).add(a*2 + dA);
-			}
-//			uf = new UnionFind(n);
-			Arrays.fill(roots,-1);
-			stack.clear();
-			visited.clear();
-			Arrays.fill(num,-1);
-			Arrays.fill(low,-1);
-			scc = 1;
-			for(int i = 0;i < n;i++)
-				if(num[i] == -1)
-					dfs(i);
-			yes = true;
-			for(int i = 0;i < n && yes;i += 2)
-				if(/*roots[i] != -1 && roots[i^1] != -1 &&*/ roots[i] == roots[i^1])
-					yes = false;
-			System.out.println(yes ? "yes":"no");
+			n = getInt();
+			Sat sat = new Sat(getInt());
+			while(n-- > 0)	sat.add(getInt(),getInt());
+			System.out.println(sat.solve() ? "yes":"no");
 		}
 	}
-	static int[] num = new int[1000],low = new int[1000],roots = new int[1000];
-	static Stack<Integer> stack = new Stack<Integer>();
-	static BitSet visited = new BitSet(1000);
-	static int scc;
-	static void dfs(int u) {
-		low[u] = num[u] = scc++;
-		visited.set(u);
-		stack.push(u);
-		for(Integer v:graph.get(u)) {
-			if(num[v] == -1)	dfs(v);
-			if(visited.get(v))	low[u] = Math.min(low[u],low[v]);
+	static class Sat {
+		private ArrayList<ArrayList<Integer>> graph;
+		private Stack<Integer> stack;
+		private BitSet visited;
+		private int[] num,low,head;
+		private int size,numscc;
+
+		public Sat(int sz) {
+			size = sz * 2;
+			graph = new ArrayList<ArrayList<Integer>>(size);
+			for(int i = 0;i < size;i++)
+				graph.add(new ArrayList<Integer>());
 		}
-		if(low[u] == num[u]) {
-			while(!stack.isEmpty()) {
-				int v = stack.pop();
-				roots[v] = u;
-//				uf.merge(u,v);
-				visited.set(v,false);
-				if(u == v)	break;
+		public void add(int a,boolean ba,int b,boolean bb) {
+			a--; b--;
+			graph.get((a*2) ^ btoi(ba^true)).add((b*2) ^ btoi(bb));
+			graph.get((b*2) ^ btoi(bb^true)).add((a*2) ^ btoi(ba));
+		}
+		public void add(int a,int b) {add(Math.abs(a),a > 0,Math.abs(b),b > 0);}
+		private int btoi(boolean b) {return b ? 1:0;}
+
+		public boolean solve() {
+			stack = new Stack<Integer>(); visited = new BitSet(size);
+			head = new int[size]; num = new int[size]; low = new int[size];
+			Arrays.fill(num,-1); Arrays.fill(low,-1); Arrays.fill(head,-1);
+			for(int i = 0;i < size;i++) if(num[i] == -1)
+				dfs(i);
+			for(int i = 0;i < size;i += 2)
+				if(head[i] != -1 && head[i^1] != -1 && head[i] == head[i^1])
+					return false;
+			return true;
+		}
+		private void dfs(int u) {
+			low[u] = num[u] = numscc++;
+			visited.set(u); stack.push(u);
+			for(Integer v:graph.get(u)) {
+				if(num[v] == -1)	dfs(v);
+				if(visited.get(v))	low[u] = Math.min(low[u],low[v]);
+			}
+			if(low[u] == num[u]) {
+				while(!stack.isEmpty()) {
+					int v = stack.pop();
+					head[v] = u;
+					visited.set(v,false);
+					if(u == v)	break;
+				}
 			}
 		}
 	}
-	static ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>(1000);
 }
--- UVa10319 - Manhattan [SCC]/src/Main.java
+++ UVa10319 - Manhattan [SCC]/src/Main.java
@@ -15,61 +15,70 @@ public class Main {
 		try {System.setIn(new FileInputStream("in.txt"));} catch(Exception e) {}
 		st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
 		int TC = getInt();
-		for(int i = 0;i < MAXN;i++)	graph.add(new ArrayList<Integer>());
 		while(TC-- > 0) {
-			int nS = getInt(),nA = getInt(),m = getInt(),n = (nS + nA) * 2;
-			for(ArrayList<Integer> each:graph)	each.clear();
+			int nS = getInt(),nA = getInt(),m = getInt();
+			Sat sat = new Sat(nS + nA);
 			while(m-- > 0) {
 				int s1 = getInt() - 1,a1 = getInt() + nS - 1,s2 = getInt() - 1,a2 = getInt() + nS - 1;
 				int distS = s1 - s2,distA = a1 - a2;
 				if(distS == 0 && distA == 0)	continue;
-				int dirS = distA > 0 ? 1:0;
-				int dirA = distS > 0 ? 1:0;
-				if(distS == 0)
-					add(s1,dirS ^ 1,s1,dirS);
-				else if(distA == 0)
-					add(a1,dirA ^ 1,a1,dirA);
+				boolean dirS = distA > 0,dirA = distS > 0;
+				if(distS == 0)		sat.add(s1,dirS,s1,dirS);
+				else if(distA == 0)	sat.add(a1,dirA,a1,dirA);
 				else {
-					add(s1,dirS^1,s2,dirS);	add(s1,dirS^1,a1,dirA);
-					add(a2,dirA^1,s2,dirS);	add(a2,dirA^1,a1,dirA);
-					add(s2,dirS^1,s1,dirS);	add(s2,dirS^1,a2,dirA);
-					add(a1,dirA^1,s1,dirS);	add(a1,dirA^1,a2,dirA);
+					sat.add(s1,dirS,s2,dirS); sat.add(s1,dirS,a1,dirA);
+					sat.add(a2,dirA,s2,dirS); sat.add(a2,dirA,a1,dirA);
+					sat.add(s2,dirS,s1,dirS); sat.add(s2,dirS,a2,dirA);
+					sat.add(a1,dirA,s1,dirS); sat.add(a1,dirA,a2,dirA);
 				}
 			}
-			stack.clear();
-			visited.clear();
-			Arrays.fill(num,-1);
-			Arrays.fill(low,-1);
-			scc = 1;
-			for(int i = 0;i < n;i++)	if(num[i] == -1)	dfs(i);
-			boolean yes = true;
-			for(int i = 0;i < n && yes;i += 2)
-				if(roots[i] == roots[i^1])
-					yes = false;
-			System.out.println(yes ? "Yes":"No");
+			System.out.println(sat.solve() ? "Yes":"No");
 		}
 	}
-	static int[] num = new int[MAXN],low = new int[MAXN],roots = new int[MAXN];
-	static Stack<Integer> stack = new Stack<Integer>();
-	static BitSet visited = new BitSet(1000);
-	static int scc;
-	static void dfs(int u) {
-		low[u] = num[u] = scc++;
-		visited.set(u);
-		stack.push(u);
-		for(Integer v:graph.get(u)) {
-			if(num[v] == -1)	dfs(v);
-			if(visited.get(v))	low[u] = Math.min(low[u],low[v]);
+	static class Sat {
+		private ArrayList<ArrayList<Integer>> graph;
+		private Stack<Integer> stack;
+		private BitSet visited;
+		private int[] num,low,head;
+		private int size,numscc;
+		public Sat(int sz) {
+			size = sz * 2;
+			graph = new ArrayList<ArrayList<Integer>>(size);
+			for(int i = 0;i < size;i++)
+				graph.add(new ArrayList<Integer>());
 		}
-		if(low[u] == num[u]) {
-			while(!stack.isEmpty()) {
-				int v = stack.pop();
-				roots[v] = u;
-				visited.set(v,false);
-				if(u == v)	break;
+		public void add(int a,boolean ba,int b,boolean bb) {
+			a--; b--;
+			graph.get((a*2) ^ btoi(ba^true)).add((b*2) ^ btoi(bb));
+			graph.get((b*2) ^ btoi(bb^true)).add((a*2) ^ btoi(ba));
+		}
+		public void add(int a,int b) {add(Math.abs(a),a > 0,Math.abs(b),b > 0);}
+		private int btoi(boolean b) {return b ? 1:0;}
+		public boolean solve() {
+			stack = new Stack<Integer>(); visited = new BitSet(size);
+			head = new int[size]; num = new int[size]; low = new int[size];
+			Arrays.fill(num,-1); Arrays.fill(low,-1); Arrays.fill(head,-1);
+			for(int i = 0;i < size;i++) if(num[i] == -1) dfs(i);
+			for(int i = 0;i < size;i += 2)
+				if(head[i] != -1 && head[i^1] != -1 && head[i] == head[i^1])
+					return false;
+			return true;
+		}
+		private void dfs(int u) {
+			low[u] = num[u] = numscc++;
+			visited.set(u); stack.push(u);
+			for(Integer v:graph.get(u)) {
+				if(num[v] == -1)	dfs(v);
+				if(visited.get(v))	low[u] = Math.min(low[u],low[v]);
+			}
+			if(low[u] == num[u]) {
+				while(!stack.isEmpty()) {
+					int v = stack.pop();
+					head[v] = u;
+					visited.set(v,false);
+					if(u == v)	break;
+				}
 			}
 		}
 	}
-	static void add(int from,int f,int to,int t) {graph.get(from*2 + f).add(to*2 + t);}
-	static ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>(MAXN);
 }
