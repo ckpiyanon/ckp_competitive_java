e50364d6eddb430107553aea81a9ef80505cc9b8
Chinakrit Lorpiyanon <Chinakrit Lorpiyanon>
2016-11-01 21:48:24 +0700

    Faster UVa00417 - Word Index

--- UVa00417 - Word Index/src/Main.java
+++ UVa00417 - Word Index/src/Main.java
@@ -1,28 +1,27 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Main {
-	static HashMap<String,Integer> map = new HashMap<String,Integer>();
-	static void per(int x,int n,String str) {
-		if(n == 0) {
-			map.put(str,map.size() + 1);
-			return;
-		}
-		for(int i = x;i < 26;i++)	per(i + 1,n - 1,str + (char)('a' + i));
+	static Map<String,Integer> map = new TreeMap<String,Integer>();
+	static int counter = 0;
+	static void permute(int x,String s,int max) {
+		if(s.length() == max)	map.put(s,++counter);
+		else	while(x < 26)	permute(x+1,s + (char)(x++ + 'a'),max);
 	}
 	public static void main(String args[]) throws Exception {
+		try {System.setIn(new FileInputStream("in.txt"));} catch(Exception e) {}
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
 		String s;
-		Integer ans;
-		for(int i = 1;i <= 5;i++)	per(0,i,"");
+		for(int i = 1;i <= 5;i++)	permute(0,"",i);
 		while((s = in.readLine()) != null) {
-			ans = map.get(s);
-			out.write(ans == null ? "0":ans.toString());
-			out.write('\n');
+			Integer ans = map.get(s);
+			out.write(ans == null ? "0\n":ans + "\n");
 		}
 		out.flush();
 	}
