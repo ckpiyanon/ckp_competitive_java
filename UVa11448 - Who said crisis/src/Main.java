42caa8129cfaa27a517beac1d114bb64b19017ec
Chinakrit Lorpiyanon <Chinakrit Lorpiyanon>
2016-12-26 02:10:19 +0700

    Accepted UVa11448 - Who said crisis?

---
+++ UVa11448 - Who said crisis/src/Main.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String args[]) throws Exception {
+		try {System.setIn(new FileInputStream("in.txt"));} catch(Exception e) {}
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		int TC = Integer.parseInt(in.readLine());
+		while(TC-- > 0) {
+			StringTokenizer st = new StringTokenizer(in.readLine());
+			out.write(new BigInteger(st.nextToken()).subtract(new BigInteger(st.nextToken())) + "\n");
+		}
+		out.flush();
+	}
+}
