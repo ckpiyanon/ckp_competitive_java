c902fca76f32ad9bb9b36d13a08bf132b272b059
Chinakrit Lorpiyanon <Chinakrit Lorpiyanon>
2017-01-07 20:50:37 +0700

    Accepted UVa12502 - Three Families

---
+++ UVa12502 - Three Families/src/Main.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.StreamTokenizer;
+
+public class Main {
+	static StreamTokenizer st;
+	static int getInt() throws Exception {st.nextToken(); return (int)st.nval;}
+	public static void main(String args[]) throws Exception {
+//		try {System.setIn(new FileInputStream("in.txt"));} catch(Exception e) {}
+		st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		int TC = getInt(),x,y,z;
+		while(TC-- > 0) {
+			x = getInt(); y = getInt(); z = getInt();
+			out.write(z * (x - y + x) / (x + y) + "\n");
+		}
+		out.flush();
+	}
+}
